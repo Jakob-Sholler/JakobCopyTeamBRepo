5b9690b87f1d12236c738676f98ee69cd81c681e
Dmitrii Tsutskov <89928237+yTKONOSINA@users.noreply.github.com>
2024-11-09 18:18:20 -0400

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   presets: [
-    ['@babel/preset-env', {targets: {node: 'current'}}],
-    '@babel/preset-typescript',
+    '@babel/preset-env', // For transforming modern JavaScript
+    '@babel/preset-react', // For React support (if using React)
+    '@babel/preset-typescript', // For TypeScript support
   ],
 };
