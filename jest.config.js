1bfb804062ff2a037e3240e85cb302702f51981a
Dmitrii Tsutskov <89928237+yTKONOSINA@users.noreply.github.com>
2024-11-09 18:21:52 -0400

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,20 @@
-module.exports = {
+import type { Config } from 'jest';
+
+const config: Config = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   transform: {
-    '^.+\\.(ts|tsx|js|jsx)$': 'babel-jest',
+    '^.+\\.(ts|tsx|js|jsx)$': 'babel-jest',  // Transpile TypeScript and JavaScript with Babel
   },
-    globals: {
+  globals: {
     'ts-jest': {
-      tsconfig: 'tsconfig.json', // Ensure ts-jest uses the correct TypeScript config
+      tsconfig: 'tsconfig.json',  // Ensure ts-jest uses the correct TypeScript config
     },
   },
   transformIgnorePatterns: [
-    '/node_modules/(?!firebase)', // Optional, for packages needing transformation
+    '/node_modules/(?!firebase)',  // Optional, for packages needing transformation
   ],
+  verbose: true,  // Set Jest to be more verbose in the output
 };
+
+export default config;
