be821ab9b6ad49fd89715f1035615805e5a1576e
Dmitrii Tsutskov <89928237+yTKONOSINA@users.noreply.github.com>
2024-11-09 18:17:11 -0400

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,11 @@ module.exports = {
   transform: {
     '^.+\\.(ts|tsx|js|jsx)$': 'babel-jest',
   },
+    globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.json', // Ensure ts-jest uses the correct TypeScript config
+    },
+  },
   transformIgnorePatterns: [
     '/node_modules/(?!firebase)', // Optional, for packages needing transformation
   ],
