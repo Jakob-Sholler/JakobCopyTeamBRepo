dbc27299d840017d1df65e9ad8055ed9fe5dc910
Dmitrii Tsutskov <89928237+yTKONOSINA@users.noreply.github.com>
2024-11-09 18:33:21 -0400

    Update and rename jest.config.mjs to jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,6 @@
 import type { Config } from 'jest';
 
 const config: Config = {
-  verbose: true,
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   transform: {
@@ -10,11 +9,11 @@ const config: Config = {
   },
   globals: {
     'ts-jest': {
-      tsconfig: 'tsconfig.json', // Ensure ts-jest uses the correct TypeScript config
+      tsconfig: 'tsconfig.json',
     },
   },
   transformIgnorePatterns: [
-    '/node_modules/(?!firebase)', // Optional, for packages needing transformation
+    '/node_modules/(?!firebase)',
   ],
 };
 
