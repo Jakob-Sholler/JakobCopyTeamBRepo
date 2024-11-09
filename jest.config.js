50b558c5ddf04aa2e5294d0525229d3eec01ae87
Dmitrii Tsutskov <89928237+yTKONOSINA@users.noreply.github.com>
2024-11-09 18:04:23 -0400

    Create jest.config.js

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.(ts|tsx|js|jsx)$': 'babel-jest',
+  },
+  transformIgnorePatterns: [
+    '/node_modules/(?!firebase)', // Optional, for packages needing transformation
+  ],
+};
